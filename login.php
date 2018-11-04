2fcf6502ef5983a6d5666168c8c8bfd914e176df
StevenVroman <32570706+StevenVroman@users.noreply.github.com>
2018-11-04 20:36:59 +0100

    4-11

--- login.php
+++ login.php
@@ -6,29 +6,35 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossorigin="anonymous">
+     <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/screen.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
     <title>login</title>
     
 </head>
-<body>
-<div class="login">
+<body id="loginpage">
+<div id="login">
     <div class="container">
         <div class="col-12 login-form-2">
                     <h3>Login</h3>
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Your Email *" value="" />
+                            <input type="text" class="form-control" placeholder="Username" value="" />
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Your Password *" value="" />
+                            <input type="password" class="form-control" placeholder="Password" value="" />
                         </div>
-                        <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Login" />
+                        <div class="container form-group">
+                        <div class="row">
+                            <div class="col-6">
+                                <input type="submit" class="btnSubmit" value="Login" />
+                            </div>
+                             <div class="col-6">
+                                <input type="submit" class="btnregis" value="Registreer" />
+                             </div>
+                            <div class="col-12">
+                                <a href="#" class="btnForgetPwd" value="Login">Forgot Password?</a>
+                            </div>
                         </div>
-                        <div class="form-group">
 
-                            <a href="#" class="btnForgetPwd" value="Login">Forgot Password?</a>
-                        </div>
         </div>
     </div>
 </div>
