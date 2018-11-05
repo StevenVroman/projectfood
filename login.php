41deb6328d72777298d8e36b67a4daf3a5b3a991
StevenVroman <32570706+StevenVroman@users.noreply.github.com>
2018-11-06 00:46:09 +0100

    5/11 end

--- login.php
+++ login.php
@@ -2,8 +2,37 @@
  include_once 'scripts/config.php';?>
 <?php include_once 'scripts/api.php';
 $users = CallAPI("GET", $DB2."/tblusers");
-print_r($users);
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+$wrongpass = false;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['submit'])) {
+        //login button
+        if(isset($_POST['login']) && isset($_POST['pass']))
+        {
+        $filledinlogin=$_POST['login'];
+        $filledinpass=$_POST['pass'];
+        foreach($users as $user){
+            if($filledinlogin==$user["Username"] && $filledinpass==$user["Pass"])
+            {
+                //login is ok // heeft nog geen cookie //aanmaken en doorsturen
+                print("check");
+            }
+            else{
+
+                $wrongpass=true;
+            }
+        }
+
+        }
+
+    } 
+    elseif(isset($_POST['forgot'])) {
+       //kader komt met voorbeeldaccount
+
+    }
+    
+}
+
 
 
 ?>
@@ -24,29 +53,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 <div id="login">
     <div class="container">
         <div class="col-12 login-form-2">
-            <img src="pics/logo.png" alt="logo" id="logo">
-                    <form action ="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+                <div id="logopic">
+                <img src="pics/logo.png" alt="logo"> 
+                </div>
+                    <form action ="<?php $_SERVER['PHP_SELF'] ?>" method="POST" id="loginform">
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Username" value="" required/>
+                            <input type="text" class="form-control" placeholder="Username" value="" required name="login"/>
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Password" value="" required />
+                            <input type="password" class="form-control" placeholder="Password" value="" name="pass" required />
                         </div>
                         <div class="container form-group">
                         <div class="row">
                             <div class="col-6">
-                                <input type="submit" class="btnSubmit form-control" value="Login" />
+                                <input type="submit" class="btnSubmit form-control" value="Login" name="submit"/>
                             </div>
                              <div class="col-6">
-                                <input type="submit" class="btnregis form-control" value="Registreer" />
+                                <input type="submit" class="btnregis form-control" value="Registreer" name="Registreer" />
                              </div>
                             <div class="col-12">
-                                <a href="#" class="btnForgetPwd" value="Login">Forgot Password?</a>
+                                <a href="#" class="btnForgetPwd">Forgot Password?</a>
                             </div>
+                        </div>
                         </div>
                     </form>
         </div>
     </div>
 </div>
 </body>
+<script>
+
+   var verkeerdpas = "<?php echo $wrongpass; ?>";
+   if (verkeerdpas==true){
+       //message tonen dat niet overeenkomt
+       para = document.createElement("h3");
+        node = document.createTextNode("Username and or Password doestn match" );
+        para.appendChild(node);
+       document.getElementById("logopic").insertBefore(para, null);
+   }
+</script>
 </html>
